c9ef1ed0e5c260a5ae1ea9128ca66470b2ba95e5
Thomaszz4 <54606455+Thomaszz4@users.noreply.github.com>
2021-01-04 16:57:21 +0800

    Update activation_maximization.py

--- activation_maximization.py
+++ activation_maximization.py
@@ -1,3 +1,7 @@
+"""
+the implementation of Activation Maximization:
+"""
+
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
@@ -13,10 +17,12 @@ x_train = x_train.reshape((60000, 28, 28, 1)) / 255
 
 X_prototype = tf.random.normal(shape=(10, 28, 28, 1))
 Y_prototype = tf.one_hot(tf.cast(tf.linspace(0., 9., 10), tf.int32), depth=10)
+# note! tf.Variable can be used for Automatic derivation in tensorflow. Remember to convert!
 X_prototype = tf.Variable(X_prototype)
 Y_prototype = tf.Variable(Y_prototype)
 lmda = 0.1
 
+# plot the random images
 # fig, axs = plt.subplots(2, 5)
 # fig.suptitle('Random img')
 # num = 0
@@ -41,14 +47,15 @@ for epoch in range(1000):
     opt.minimize(loss, var_list=[X_prototype])
     print("this is %d epoch, the loss is %f" % (epoch, loss()))
 
+# plot and save the activation maximization images
 fig, axs = plt.subplots(2, 5)
 fig.suptitle('Activation Maximization')
 num = 0
-for i in axs:
-    for j in i:
+for row in axs:
+    for col in row:
         if num <= 9:
-            j.imshow(X_prototype[num])
-            j.set_title('number' + str(num))
+            col.imshow(X_prototype[num])
+            col.set_title('number' + str(num))
         num += 1
 plt.savefig('a.jpg')
 plt.show()
